2860df07fae57d7d82a33a9d71749928d1a010a2
AndresJimw <brandrez.474@gmail.com>
2025-02-01 14:27:54 -0500

    fix: updadate packages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig, loadEnv } from "vite";
-import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
@@ -17,10 +16,6 @@ export default defineConfig(({ mode }) => {
 		plugins: [
 			react(),
 			tsconfigPaths(),
-			createSvgIconsPlugin({
-				iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
-				symbolId: "icon-[dir]-[name]",
-			}),
 			isProduction &&
 				visualizer({
 					open: true,
